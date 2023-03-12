88b34ef37b460738b34560f00911238ba0b2ecdd
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-03-12 16:05:12 +0100

    added steps section

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -5,20 +5,20 @@ const swiper = new Swiper('.swiper', {
 
     slidesPerView: 1,
     //slidesPerView: 1.5,
-    spaceBetween: 0,
+    spaceBetween: 30,
 
     breakpoints: {
         640: {
             slidesPerView: 1,
-            spaceBetween: 0,
+            spaceBetween: 30,
         },
         768: {
             slidesPerView: 2,
-            spaceBetween: 0,
+            spaceBetween: 30,
         },
         1024: {
-            slidesPerView: 4,
-            spaceBetween: 0,
+            slidesPerView: 3.5,
+            spaceBetween: 30,
         },
     },
 
