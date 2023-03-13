ff515fa6e909feae43c39629bea46686a02a5e70
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-03-13 15:26:47 +0100

    made the home page responsive

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -1,25 +1,47 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     direction: 'horizontal',
     loop: false,
 
     slidesPerView: 1,
-    //slidesPerView: 1.5,
-    spaceBetween: 30,
+    spaceBetween: 10,
 
     breakpoints: {
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 30,
+        380: {
+            slidesPerView: 1.2,
+            spaceBetween: 10,
         },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
+        500: {
+            slidesPerView: 1.5,
+            spaceBetween: 10,
+        },
+        640: {
+            slidesPerView: 1.7,
+            spaceBetween: 20,
+        },
+        815: {
+            slidesPerView: 2.3,
+            spaceBetween: 20,
         },
         1024: {
-            slidesPerView: 4,
+            slidesPerView: 2.5,
             spaceBetween: 30,
         },
+        1100:{
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1200: {
+            slidesPerView: 3.2,
+            spaceBetween: 30,
+        },
+        1300: {
+            slidesPerView: 3.4,
+            spaceBetween: 30,
+        },
+        1400: {
+            slidesPerView: 3.9,
+            spaceBetween: 30,
+        }
     },
 
     // If we need pagination
@@ -28,61 +50,53 @@ const swiper = new Swiper('.swiper', {
         clickable: true
     }
 });
-
-const handleNext = () => {
-    swiper.slideNext();
-}
-
-const handlePrev = () => {
-    swiper.slidePrev();
-}
-
-let next = document.querySelector('#mynext')
-next.addEventListener('click', handleNext)
 
 const swiper2 = new Swiper('.swiper2', {
-    // Optional parameters
     direction: 'horizontal',
     loop: false,
 
-    slidesPerView: 1,
-    //slidesPerView: 1.5,
-    spaceBetween: 30,
+    slidesPerView: 1.5,
+    spaceBetween: 10,
 
     breakpoints: {
         640: {
-            slidesPerView: 1,
-            spaceBetween: 30,
+            slidesPerView: 2,
+            spaceBetween: 10,
         },
         768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
+            slidesPerView: 3,
+            spaceBetween: 20,
         },
         1024: {
             slidesPerView: 4,
             spaceBetween: 30,
         },
     },
-
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true
     }
 });
 
+var menu = ['Slide 1', 'Slide 2', 'Slide 3']
 const swiper3 = new Swiper('.swiper3', {
     direction: 'horizontal',
     loop: true,
     centeredSlides: true,
     freeMode: false,
     autoplay: {
-        delay: 0,
+        delay: 3000,
         disableOnInteraction: false
     },
-    speed: 3000,
-    slidesPerView: 5,
-    spaceBetween: 30,
+    speed: 500,
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '" style="background: url(./images/Surface1.png); background-size: cover; background-position: center;"></span>';
+        },
+    }
 });
 
 const swiper4 = new Swiper('.swiper4', {
@@ -97,30 +111,43 @@ const swiper4 = new Swiper('.swiper4', {
 });
 
 const swiper5 = new Swiper('.swiper5', {
-    // Optional parameters
     direction: 'horizontal',
     loop: false,
 
-    slidesPerView: 1,
+    slidesPerView: 1.2,
     //slidesPerView: 1.5,
-    spaceBetween: 30,
+    spaceBetween: 10,
 
     breakpoints: {
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 30,
+        360: {
+            slidesPerView: 1.5,
+            spaceBetween: 10,
         },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
+        600: {
+            slidesPerView: 2.5,
+            spaceBetween: 10,
+        },
+        700: {
+            slidesPerView: 3,
+            spaceBetween: 20,
         },
         1024: {
-            slidesPerView: 5,
+            slidesPerView: 3.5,
+            spaceBetween: 20,
+        },
+        1100: {
+            slidesPerView: 4,
             spaceBetween: 30,
         },
+        1300: {
+            slidesPerView: 4.5,
+            spaceBetween: 30,
+        },
+        1400: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        }
     },
-
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true
