28489fec3d748dc2d37ec0958925e8ad1a418e29
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-03-13 22:20:19 +0100

    deploying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./public/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
