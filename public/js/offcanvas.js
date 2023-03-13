665827fd51aa88bcbbfe9cb0bc1cd2a479683c5d
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-03-13 22:15:06 +0100

    added overflow-hidden to body when offcanvas is opened

--- public/js/offcanvas.js
+++ public/js/offcanvas.js
@@ -3,11 +3,12 @@ const showMenu = () => {
 
     if (menuOffcanvas.classList.contains('invisible')) {
         menuOffcanvas.classList.remove('invisible')
+        document.getElementById('body').classList.add('overflow-hidden')
     }
 }
 
 const hideMenu = () => {
     const menuOffcanvas = document.getElementById('menuOffcanvas');
-
     menuOffcanvas.classList.add('invisible')
+    document.getElementById('body').classList.remove('overflow-hidden')
 }
