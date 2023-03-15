f76594c44a39702daf23ed954fd6b0caab2300f1
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-03-15 22:34:03 +0100

    added top nav dropdown to listing page

--- public/js/dropDowns.js
+++ public/js/dropDowns.js
@@ -1,22 +1,73 @@
+const addHidden = () => {
+    const allTopNavDropDown = document.querySelectorAll('.allTopNavDropDown');
+
+    allTopNavDropDown.forEach(topNav => {
+        if (topNav.classList.contains('hidden')) {
+            return
+        } else {
+            topNav.classList.add('hidden')
+        }
+    });
+}
+
+window.onclick = (e) => {
+    console.log(e.target)
+    if (e.target.classList.contains('dropDownBtn')) {
+    } else {
+        addHidden();
+    }
+}
+
 const showListingDropDown = () => {
+    addHidden();
     const listingDropDown1 = document.getElementById('listingDropDown1')
 
     listingDropDown1.classList.toggle('hidden')
 }
 
 const showPagesDropDown = () => {
+    addHidden();
     const pagesDropDown1 = document.getElementById('pagesDropDown1')
 
     pagesDropDown1.classList.toggle('hidden')
 }
 
-/*window.onclick = (e) => {
-    console.log(e.target)
-    if (!e.target.matches('.dropDownBtn')) {
-        console.log("weh")
-        addHidden();
-    }
-}*/
+const showPropertyDropDown = () => {
+    addHidden();
+    const propertyDropDown1 = document.getElementById('propertyDropDown1')
+
+    propertyDropDown1.classList.toggle('hidden')
+}
+
+const showListingDropDown2 = () => {
+    addHidden();
+    const listingDropDown3 = document.getElementById('listingDropDown3')
+
+    listingDropDown3.classList.toggle('hidden')
+}
+
+const showPagesDropDown2 = () => {
+    addHidden();
+    const pagesDropDown3 = document.getElementById('pagesDropDown3')
+
+    pagesDropDown3.classList.toggle('hidden')
+}
+
+const showPropertyDropDown2 = () => {
+    addHidden();
+    const propertyDropDown3 = document.getElementById('propertyDropDown3')
+
+    propertyDropDown3.classList.toggle('hidden')
+}
+
+
+
+
+
+
+
+
+
 
 const showListingAccordion = () => {
     const listingDropDown2 = document.getElementById("listingDropDown2");
