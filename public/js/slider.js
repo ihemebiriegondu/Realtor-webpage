2ab669081832b42fc47cc788cc77eb900e0e2c28
ihemebiriegondu <ihemebiriegondu@gmail.com>
2023-03-17 22:54:08 +0100

    added range slider

---
+++ public/js/slider.js
@@ -0,0 +1,22 @@
+window.addEventListener('my-custom-range-changed-event', (e) => { 
+    const data = e.detail; 
+    //console.log(data)
+
+    const minRangeValue = document.getElementById('minRangeValue')
+    const maxRangeValue = document.getElementById('maxRangeValue')
+
+    minRangeValue.value = data.minRangeValue
+    maxRangeValue.value = data.maxRangeValue
+});
+
+window.addEventListener('my-custom-range-changed-event2', (e) => { 
+    const data = e.detail; 
+    console.log(data)
+
+    const minRangeValue = document.getElementById('minRangeValue2')
+    const maxRangeValue = document.getElementById('maxRangeValue2')
+
+    minRangeValue.value = data.minRangeValue
+    maxRangeValue.value = data.maxRangeValue
+});
+
